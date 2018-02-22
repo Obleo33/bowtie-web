2654a9520f3a084667ee7b35fd983d40628312f6
Franklin Crosby <obleo33@gmail.com>
2018-02-22 16:04:09 -0700

    Add basic component structure for Login, Projects and Todos

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import Login from './Login'
+import Projects from './Projects'
+import Todos from './Todos'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      userName:'',
+      projects:'',
+      todos:'',
+    }
+  }
+
+  handleLogin() {
+    
+  }
+
+  handleProjectClick() {
+
+  }
+
+  handeleTodo() {
+
+  }
+
   render() {
     return (
       <div className="App">
-        <nav>Please login to start tracking your todos</nav>
+        <nav>
+          <p>Please login to start tracking your todos</p>
+          <Login handleLogin={this.handleLogin.bind(this)}/>
+        </nav>
+        <Projects handleProjectClick={this.handleProjectClick.bind(this)}/>
+        <Todos handeleTodo={this.handeleTodo.bind(this)}/>
       </div>
     );
   }
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+
+
+	return (
+		<div>I am the Login component</div>
+	)
+}
+
+export default Login;
