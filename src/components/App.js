0be309ab8e6a7ec2f9addbaae1d2eebb4e60c447
Franklin Crosby <obleo33@gmail.com>
2018-02-22 14:54:59 -0700

    Adjust file structure and add files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav>Please login to start tracking your todos</nav>
+      </div>
+    );
+  }
+}
+
+export default App;
